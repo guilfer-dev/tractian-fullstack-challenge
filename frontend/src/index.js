c8518406ef4405469664ac9a1124cc02a43f7b7c
Guilherme Fernandes <57098649+guilfer-dev@users.noreply.github.com>
2021-11-12 00:08:57 -0300

    [feat] Init frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <p>Hello World!</p>
-    <p>Don't forget to add your metadata on index.html and manifest.json</p>
+    <App></App>
   </React.StrictMode>,
   document.getElementById('root')
 );
